07debd979729fd4805e102ae1f8f21070217365a
Sam Phillips <samdphillips@gmail.com>
2022-12-02 11:27:11 -0800

    Rewrite day 2 to use Minikanren

--- 02.rkt
+++ 02.rkt
@@ -1,6 +1,8 @@
 #lang racket
 
 (require advent-of-code
+         minikanren
+         syntax/parse/define
          threading)
 
 (define input-stream
@@ -8,50 +10,72 @@
        (in-port read-line)
        sequence->stream))
 
-(define a-plays '(["A" . rock] ["B" . paper] ["C" . scissors]))
+(define l '(rock paper scissors))
+(define w '(paper scissors rock))
 
-(define (play-score play)
-  (length (memq play '(scissors paper rock))))
+(defrel (caro c a) (fresh (d) (== c (cons a d))))
+(defrel (cdro c d) (fresh (a) (== c (cons a d))))
+(defrel (nullo c) (== c null))
 
-(define (outcome-score a b)
-  (match* {a b}
-    [{'rock 'scissors} 0]
-    [{'scissors 'paper} 0]
-    [{'paper 'rock} 0]
-    [{'scissors 'rock} 6]
-    [{'paper 'scissors} 6]
-    [{'rock 'paper} 6]
-    [{_ _} 3]))
+(defrel (lookup them us a b)
+  (conde
+   [(nullo a) (== #t #f)]
+   [(fresh (a0 b0)
+      (caro a a0) (caro b b0)
+      (== a0 them) (== b0 us))]
+   [(fresh (a* b*)
+      (cdro a a*) (cdro b b*)
+      (lookup them us a* b*))]))
+
+(defrel (roshambo them us outcome)
+  (conde
+   [(== them us) (== outcome 'draw)]
+   [(== outcome 'win) (lookup them us l w)]
+   [(== outcome 'lose) (lookup them us w l)]))
+
+(defrel (play-score play outcome score)
+  (fresh (pscore oscore)
+    (conde
+     [(== play 'rock) (== pscore 1)]
+     [(== play 'paper) (== pscore 2)]
+     [(== play 'scissors) (== pscore 3)])
+    (conde
+     [(== outcome 'win) (== oscore 6)]
+     [(== outcome 'draw) (== oscore 3)]
+     [(== outcome 'lose) (== oscore 0)])
+    (project (pscore oscore)
+      (== score (+ pscore oscore)))))
+
+(define them-plays '(["A" . rock] ["B" . paper] ["C" . scissors]))
+
+(define-syntax-parse-rule (runv (x:id ...) g ...)
+  (apply values (run 1 (x ...) g ...)))
 
 (module* part1 #f
-  (define b-plays '(["X" . rock] ["Y" . paper] ["Z" . scissors]))
+  (define us-plays '(["X" . rock] ["Y" . paper] ["Z" . scissors]))
 
   (define (decode-line s)
     (match-define (regexp #px"^(.).(.)$" (list _ a b)) s)
-    (values (dict-ref a-plays a) (dict-ref b-plays b)))
+    (values (dict-ref them-plays a) (dict-ref us-plays b)))
 
   (for/sum ([r input-stream])
-    (define-values (a b) (decode-line r))
-    (+ (play-score b) (outcome-score a b))))
+    (define-values (them us) (decode-line r))
+    (runv (score)
+      (fresh (outcome)
+        (roshambo them us outcome)
+        (play-score us outcome score)))))
 
 (module* part2 #f
   (define results '(["X" . lose] ["Y" . draw] ["Z" . win]))
 
-  (define losing '([rock . scissors] [paper . rock] [scissors . paper]))
-  (define winning '([rock . paper] [paper . scissors] [scissors . rock]))
-
   (define (decode-line s)
     (match-define (regexp #px"^(.).(.)$" (list _ a b)) s)
-    (values (dict-ref a-plays a) (dict-ref results b)))
-
-  (define (should-play play result)
-    (match result
-      ['draw play]
-      ['lose (dict-ref losing play)]
-      ['win (dict-ref winning play)]))
+    (values (dict-ref them-plays a) (dict-ref results b)))
 
   (for/sum ([r input-stream])
-    (define-values (a result) (decode-line r))
-    (define b (should-play a result))
-    (+ (play-score b) (outcome-score a b))))
+    (define-values (them outcome) (decode-line r))
+    (runv (score)
+      (fresh (us)
+        (roshambo them us outcome)
+        (play-score us outcome score)))))
 
