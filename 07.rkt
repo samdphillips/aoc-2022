2355bb2ca43eb3703163b94bc5050f44dc05a0b8
Sam Phillips <samdphillips@gmail.com>
2022-12-07 12:09:28 -0800

    Day 7 cleanup

--- 07.rkt
+++ 07.rkt
@@ -79,40 +79,8 @@
       candidate))
   (directory-size (car (sort candidates < #:key directory-size))))
 
-(define t
-  #<<T
-$ cd /
-$ ls
-dir a
-14848514 b.txt
-8504156 c.dat
-dir d
-$ cd a
-$ ls
-dir e
-29116 f
-2557 g
-62596 h.lst
-$ cd e
-$ ls
-584 i
-$ cd ..
-$ cd ..
-$ cd d
-$ ls
-4060174 j
-8033020 d.log
-5626152 d.ext
-7214296 k
-T
-  )
-
-(define inp
-  (call-with-input-string t
-    (λ~>> (in-port read-line) sequence->stream)))
-
 (module* part1 #f
-  )
+  (find-small-directories-total (parse-input input-stream)))
 
 (module* part2 #f
-  )
+  (find-directory-to-delete (parse-input input-stream)))
